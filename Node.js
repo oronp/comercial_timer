249980ced2f3e5d2cac18beb92dd2c17a3a8c362
Oron Pariente <89079384+oronp@users.noreply.github.com>
2022-05-30 19:06:51 +0300

    finished level 2

---
+++ Node.js
@@ -0,0 +1,82 @@
+'use strict';
+const express = require("express");
+const app = express();
+const all_ads = require('./Ad_json.json');
+const fs = require('fs');
+const ads_array = require("./ads.json");
+const ads = require("./ads.json");
+app.use(express.static(__dirname));
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1> Browsing the ${subreddit} </h1>`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`<h1> Viewing the ${postId} </h1>`)
+})
+
+app.get('/', (req, res) => {
+    res.send('This is home!')
+})
+
+app.get('/screen=1', (req, res) => {
+    if(ads_by_screen(1))
+        res.sendFile(__dirname + '/main.html');
+        // res.sendFile(__dirname + ads_array[0]["templateUrl"]);
+    else
+        res.send('No ads for you today my brother');
+})
+
+app.get('/screen=2', (req, res) => {
+    if(ads_by_screen(2))
+        res.sendFile(__dirname + '/main.html');
+        // res.sendFile(__dirname + ads_array[0]["templateUrl"]);
+    else
+        res.send('No ads for you today my brother');
+})
+
+app.get('/screen=3', (req, res) => {
+    if(ads_by_screen(3))
+        res.sendFile(__dirname + '/main.html');
+        // res.sendFile(__dirname + ads_array[0]["templateUrl"]);
+    else
+        res.send('No ads for you today my brother');
+})
+
+app.post('/loadData',(req,res)=>{
+    // let ads = require('./ads.json');
+    // res.json(JSON.stringify(ads));
+    res.json(ads_array);
+});
+
+app.get('*', (req, res) => {
+    res.send('PAGE NOT EXIST!')
+})
+
+app.post('noAds', (req, res) => {
+    res.send('No ads for you today my brother')
+})
+
+app.listen(8080, () => {
+    console.log("LISTENING ON PORT 8080!")
+})
+
+// functions!
+
+function ads_by_screen(screen_number){
+    let ads_new = [];
+    // ads_new.push(all_ads["Ad1"],all_ads["Ad2"],all_ads["Ad3"]) // in case nothing match and we need to test
+    for(let i = 0; i < all_ads.amount.number; i++){
+        let ad_name = "Ad" + (i+1);
+        for(let screen of all_ads[ad_name].screens){
+            if(screen == screen_number)
+                ads_new.push(all_ads[ad_name])
+        }
+    }
+    if(ads_new.length == 0)
+        return false;
+    fs.writeFileSync('ads.json',JSON.stringify(ads_new));
+    return true;
+}
--- firstProg.js
+++ firstProg.js
@@ -21,84 +21,21 @@ class Day{
     }
 }
 
-// The ads array argument
- const ads_new = [
-     new Ad("meridian",
-         "./templateA.html",
-         ["Waterproof", "Shock resistant", "Cordless", "90 Minutes Trim Time Fully Charged"],
-         ["./pics/Meridian_balls.jpg", "./pics/Meridian_case.jpg"],
-         [new Day("monday",1,6,12),
-                   new Day("wednesday",3,13,20)],
-         [2022,0,1],
-         [2022,11,31],
-         7,
-         [1,2]
-     ),
-     new Ad("Mcdonalds",
-        "./templateB.html",
-        ["Big mac ............................................ 34nis",
-               "Mac Royal......................................... 42nis",
-               "Big America...................................... 48nis",
-               "Big Tokyo.......................................... 48nis",
-               "Big Japna.......................................... 48nis",
-               "Big Mombai....................................... 60nis",
-               "Big Texas............................................ 48nis",
-               "McNuggets........................................ 30nis",
-               "Potuto&chips..................................... 25nis",
-               "Happy Meal....................................... 34nis"],
-        ["./pics/Mac.jpg"],
-        [new Day("tuesday",2,10,16),
-               new Day("wednesday",3,10,16)],
-        [2022,2,3],
-        [2022,3,31],
-        10,
-        [1,3]
-    ),
-     new Ad("Trivago",
-         "./templateC.html",
-         ["dekel ohel batahat ve gam ahoto"], //delete it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-         [],
-         [new Day("sunday",0,8,22),
-             new Day("monday",1,8,22),
-             new Day("tuesday",2,8,22),
-             new Day("wednesday",3,8,22),
-             new Day("thursday",4,8,22),
-             new Day("friday",5,8,22),
-             new Day("saturday",6,8,22)],
-         [2022,4,1],
-         [2022,5,30], //need to change the 30 to 15
-         3,
-         [2,3]
-     ),
-     new Ad("Livnat Poran",
-         "./templateA.html",
-         ["Halita? niftsata?", "we are here for you!"],
-         [],
-         [new Day("monday",1,15,19)],
-         [2022,2,29],
-         [2022,3,15],
-         6,
-         [1]
-     ),
-     new Ad("Durex",
-         "./templateB.html",
-         ["Hey fella!",
-                "Don't forget to wear your mask",
-                "Don't limit yourself with treats",
-                "freedom means responsibility",
-                "Toys change, playtime Doesn't",
-                "Protect yourself and your love ones",
-                "Get in you closest drag store"],
-         ["./pics/sand.jpg","./pics/ps5.jpg"],
-         [new Day("monday",1,1,23),
-             new Day("tuesday",2,1,23),
-             new Day("wednesday",3,1,23)],
-         [2022,4,1],
-         [2022,4,30],
-         9,
-         [3]
-     )
- ];
+var ads_new; //will represent the right ads to publish each time.
+
+async function httpGet(){
+    let ads = '';
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:8080/loadData',
+        dataType: 'text',
+        async: false,
+        success: function(data){
+            ads = JSON.parse(data);
+            ads_new = ads;
+        }
+    })
+}
 
 function time(){
     let date_now = new Date();
@@ -113,24 +50,32 @@ function time(){
 function adSelector(){
     let ads_array = [];
     let my_time = time(); // [hour,day_in_week,day_in_month,month,year]
-    for(let i = 0; i < ads_new.length; i++){ // CHANGE IT TO FOREACH
+    let length = ads_new.length;
+    // ads_array.push(ads_new[0],ads_new[1],ads_new[2]); // in case nothing match and we need to test
+    for(let i = 0; i < length; i++){
         if(my_time[4] == ads_new[i].fromDate[0]){ // checking year
             if(my_time[3] >= ads_new[i].fromDate[1] && my_time[3] <= ads_new[i].toDate[1]){ // checking month
                 if(my_time[2] >= ads_new[i].fromDate[2] && my_time[2] <= ads_new[i].toDate[2]){ // checking day in month
-                        if(checkMyDay(my_time,ads_new[i])) // checking the day&hour in the week
-                            ads_array.push(ads_new[i]);
+                    if(checkMyDay(my_time,ads_new[i])) // checking the day&hour in the week
+                        ads_array.push(ads_new[i]);
                 }
             }
         }
     }
-    localStorage.ads = JSON.stringify(ads_array);
-    return ads_array;
+    if(ads_array.length > 0){
+        localStorage.ads = JSON.stringify(ads_array);
+        return ads_array;
+    }
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:8080/noAds'
+    })
 }
 
 function checkMyDay(time, current_ad){
-    for(let i = 0; i < current_ad.days.length; i++){
-        if(time[1] == current_ad.days[i]["number"]){
-            if(time[0] >= current_ad.days[i]["fromHour"] && time[0] < current_ad.days[i]["toHour"])
+    for(let current_day of current_ad.days){
+        if(time[1] == current_day["number"]){
+            if(time[0] >= current_day["fromHour"] && time[0] < current_day["toHour"])
                 return true;
         }
     }
@@ -138,14 +83,14 @@ function checkMyDay(time, current_ad){
 }
 
 function adsPublisher(){
-    let ads_array = JSON.parse(localStorage.ads);
-    let single_ad = ads_array[0];
-    ads_array.shift();
-    localStorage.ads = JSON.stringify(ads_array);
+    ads_new = JSON.parse(localStorage.ads);
+    let single_ad = ads_new[0];
+    ads_new.shift();
+    localStorage.ads = JSON.stringify(ads_new);
+    console.log(single_ad);
     printHead(single_ad);
     printText(single_ad);
-    // timeDelay = setTimeout(swift,single_ad["timeDuration"]*1000);
-    setTimeout(swift,single_ad["timeDuration"]*1000);
+    let timeDelay = setTimeout(swift,single_ad["timeDuration"]*1000);
 }
 
 function printHead(current_ad){
@@ -153,37 +98,36 @@ function printHead(current_ad){
 }
 
 function printText(current_ad){ // print the text and images
-    for(let elmnt of ads_new){ // CHANGE TO FOREACH
-        if(!elmnt["name"].localeCompare(current_ad["name"])){
-            var template = current_ad["templateUrl"]
-            for(let text of elmnt["texts"]){
-                document.write('<body id="body">' + text + "<br/>" + '</body>');
-            }
-            for(let img of elmnt["imageUrl"])
-            printImg(img);
-        }
+    let length = current_ad["texts"].length;
+    for(let i = 0; i < length; i++) {
+        document.write('<body id="body">' + current_ad["texts"][i] + "<br/>" + '</body>');
     }
+    for(let j = 0; j < current_ad.imageUrl.length; j++)
+        printImg(current_ad.imageUrl[j]);
 }
 
 function printImg(source){
-    var img = document.createElement("img");
+    let img = document.createElement("img");
     img.src = source;
     img.style.height = '300px';
     img.style.width = '600px';
     img.style.marginBlock = '20px';
-    var block = document.getElementById("x");
+    let block = document.getElementById("x");
     block.appendChild(img);
 }
 
-function templateSelector(){
-    let tmp = adSelector();
-    $("#templates").text(tmp[0].texts[0]).css("background-color","yellow");
-    // let ads_array = JSON.parse(localStorage.ads);
-    // document.location.href = ads_array[0]["templateUrl"];
+async function templateSelector(){
+    httpGet();
+    console.log(ads_new);
+    adSelector();
+    let ads_array = JSON.parse(localStorage.ads);
+    console.log(ads_array[0]["templateUrl"]);
+    document.location.href = ads_array[0]["templateUrl"];
 }
 
 function swift(){
     let ads_array = JSON.parse(localStorage.ads);
+    console.log(ads_array);
     if(ads_array.length != 0)
         document.location.href = ads_array[0]["templateUrl"];
     else
